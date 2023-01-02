af1b431104d1a100d64bc388bfffddc812682ba9
David Darmanger <david.darmanger@he-arc.ch>
2023-01-02 02:40:09 +0100

    add table and summary to main page

--- it/src/components/Main.vue
+++ it/src/components/Main.vue
@@ -13,6 +13,12 @@ export default {
       food_id : [],
       food : [],
       quantity : [],
+      foodItems : [],
+      totalCalories : 0,
+      totalProtein : 0,
+      totalFat : 0,
+      totalCarbs : 0,
+      //foodItems : [{id: null, name: null, quantity: null, calories: null, protein: null, fat: null, carbs: null}]
     }
   },
   created() {
@@ -94,43 +100,119 @@ export default {
             }
             })
             this.food.push(response.data[0])
+            console.log("HERE" + response.data[0].name)
+
+            // populate foodItems
+            // this.foodItems[i].id = response.data[0].id
+            // this.foodItems[i].name = response.data[0].name
+            // this.foodItems[i].quantity = this.quantity[i]
+            // this.foodItems[i].calories = response.data[0].calories
+            // this.foodItems[i].protein = response.data[0].protein
+            // this.foodItems[i].fat = response.data[0].fat
+            // this.foodItems[i].carbs = response.data[0].carbs
             }
+            // create a new object that contains the food and the quantity and calories per 100g
+            this.foodItems = this.food.map((food, i) => {
+              return {
+                id: food.id,
+                name: food.name,
+                quantity: this.quantity[i],
+                calories: food.energy_kcal,
+                protein: food.proteins_g,
+                fat: food.lipids_g,
+                carbs: food.carbohydrates_g,
+              }
+            })
+            this.summary()
             console.log(this.food)
         } catch (error) {
         console.error(error)
         }
     },
 
-    // async getMeal_UserId() {
-    //   const token = localStorage.getItem('token')
-    //   try {
-    //     const response = await axios.get(axios.defaults.baseURL + 'meal/', {
-    //       headers: {
-    //         'Authorization': `Token ${token}`,
-    //       },
-    //       params: {
-    //         id: 2,
-    //       }
-    //     })
-    //     //console.log(response.data)
-    //     } catch (error) {
-    //     console.error(error)
-    //     }
-    // },
+    async summary() {
+        for (let i = 0; i < this.foodItems.length; i++) {
+            this.totalCalories += Math.round(this.foodItems[i].calories * this.foodItems[i].quantity / 100)
+            this.totalProtein += Math.round(this.foodItems[i].protein * this.foodItems[i].quantity / 100)
+            this.totalFat += Math.round(this.foodItems[i].fat * this.foodItems[i].quantity / 100)
+            this.totalCarbs += Math.round(this.foodItems[i].carbs * this.foodItems[i].quantity / 100)
+        }
+
+    },
   }
 }
 </script>
 
 <template>
-    <div>
-        <br/>
-        <h2>Hello {{ username }} !</h2>
-        <br/>
-      <ul>
-        <li v-for="(item, index) in food" :key="item.id_food">{{ item.name + " " + item.energy_kcal * quantity[index] / 100 + " kcal" }}</li>
-      </ul>
-    </div>
+    <br/>
+    <h2 id="title">Hello <span>{{ username }}</span>  :)</h2>
+    <h4 style="text-align:center; margin-top:2cm;">Journal from {{ date }}</h4>
+    <v-table fixed-header height="300px" theme="dark" class="my-5">
+        <thead>
+            <tr>
+                <th class="text-left">
+                Name
+                </th>
+                <th class="text-left">
+                Calories
+                </th>
+                <th class="text-left">
+                Quantity
+                </th>
+                <th class="text-left">
+                Calories
+                </th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr
+                v-for="item in foodItems"
+                :key="item.name"
+            >
+                <td id="pop">{{ item.name }}</td>
+                <td>{{ item.calories }}</td>
+                <td>{{ item.quantity }}</td>
+                <td><b>{{ item.calories * item.quantity / 100 }}</b></td>
+            </tr>
+        </tbody>
+    </v-table>
+
+    <v-container class="ma-4" align="center">
+    <h2>Summary</h2>
+    <br/>
+    <v-row>
+      <v-col cols="12" sm="6" md="6">
+        <v-card class="elevation-1" color="#00B697" dark>
+          <v-card-title class="headline white--text">Total calories</v-card-title>
+          <v-card-text class="white--text">{{ totalCalories }}</v-card-text>
+        </v-card>
+      </v-col>
+      <v-col cols="12" sm="6" md="6">
+        <v-card class="elevation-1" color="#00A49E" dark>
+          <v-card-title class="headline white--text">Total protein</v-card-title>
+          <v-card-text class="white--text">{{ totalProtein }}</v-card-text>
+        </v-card>
+      </v-col>
+      <v-col cols="12" sm="6" md="6">
+        <v-card class="elevation-1" color="#0091A1" dark>
+          <v-card-title class="headline white--text">Total fat</v-card-title>
+          <v-card-text class="white--text">{{ totalFat }}</v-card-text>
+        </v-card>
+      </v-col>
+      <v-col cols="12" sm="6" md="6">
+        <v-card class="elevation-1" color="#006994" dark>
+          <v-card-title class="headline white--text">Total carbs</v-card-title>
+          <v-card-text class="white--text">{{ totalCarbs }}</v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
   </template>
   
- 
-  
+<style scoped>
+
+#pop {
+  color: #42c78c;
+  font-weight: bolder;
+}
+</style>

36d80d8e4b2751674a6b9b21038c5e3bd005458a
David Darmanger <david.darmanger@he-arc.ch>
2023-01-19 15:20:53 +0100

    plotting

--- it/src/components/History.vue
+++ it/src/components/History.vue
@@ -1,24 +1,48 @@
+<template>
+  <v-container>
+    <div id="contain">
+      <div v-if="hasMeal" id="chart">
+        <h2>Food count</h2>
+      </div>
+      <div v-else>
+        <h2>No meals registered yet</h2>
+      </div>
+      <h2 id="smallScreen">Only available for bigger screen for now :(</h2>
+    </div>
+</v-container>
+</template>
+
 <script lang="ts">
 import * as c3 from 'c3'
-import * as d3 from 'd3'
 import axios from 'axios'
 
 export default {
   data() {
     return {
       foodList: [],
-      foodCount: [
-        {name: 'concombre', value: 30},
-        {name: 'pizza quatro fromagi', value: 200},
-        {name: 'spaghetti', value: 100},
-        {name: 'kinder', value: 400},
-        {name: 'pizza margaritte', value: 150},
-        {name: 'steak de boeuf', value: 250}
-      ]
+      hasMeal: true,
     }
   },
   created() {
     this.getCount()
+  },
+  mounted() {
+    this.chart = c3.generate({
+      bindto: '#chart',
+      data: {
+        json: this.foodList,
+        keys: {
+          x: 'name',
+          value: ['count']
+        },
+        type: 'bar'
+      },
+      axis: {
+        x: {
+          type: 'category'
+        }
+      }
+    });
   },
   methods: {
     async getCount() {
@@ -31,43 +55,43 @@ export default {
         })
         this.foodList = response.data
         console.log(this.foodList)
+        //reload chart
+        this.chart.load({
+          json: this.foodList,
+          keys: {
+            x: 'name',
+            value: ['count']
+          },
+          type: 'bar'
+        })
+
         } catch (error) {
         console.error(error)
         }
     },
   },
-  mounted() {
-    let data = {
-      x: 'x',
-      columns: [
-        ['x'].concat(this.foodCount.map(item => item.name)),
-        ['data1'].concat(this.foodCount.map(item => item.value))
-      ],
-      type: 'bar'
-    };
-
-    let chart = c3.generate({
-      bindto: '#chart',
-      data: data,
-      axis: {
-        x: {
-          type: 'category',
-        }
-      }
-    });
+  beforeDestroy() {
+    this.chart.destroy();
   }
 }
 </script>
-<template>
-  <div id="contain">
-    <h2>My Chart</h2>
-    <div id="chart"></div>
-  </div>
-</template>
 
 <style scoped>
+* :not(h2){
+  font-size: 0.94em;
+}
+
 #chart {
   width: 100%;
+}
+
+#chart text {
+  stroke: white;
+  display: none;
+}
+
+.c3-axis line {
+  stroke: white;
 }
 
 #contain {
@@ -76,10 +100,37 @@ export default {
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  width: 800px;
-  padding: 2em;
+  width: 900px;
+  padding-top: 4px;
+  height: 100%;
   background-color: rgb(56, 56, 56);
-  border-radius: 20px;
+  border-radius: 8px;
+}
+
+#smallScreen {
+  display: none;
+  margin: 4px;
+}
+
+@media only screen and (max-width: 950px) {
+  #contain {
+    width: 700px;
+  }
+}
+
+@media only screen and (max-width: 700px) {
+  #contain {
+    width: 100%;
+    margin-top: 0;
+  }
+
+  #smallScreen {
+    display: block;
+  }
+
+  #chart {
+    display: none;
+  }
 }
 
 
